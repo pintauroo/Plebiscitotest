5e294171d34b1c4f8494bd4ead2b0b9e60ca1d25
tst <andreapinto1994@gmail.com>
2023-11-20 15:10:54 -0600

    Initial commit

---
+++ cluster-trace-gpu-v2020/simulator/dataset_loader.py
@@ -0,0 +1,150 @@
+import random
+import csv
+import copy
+import numpy as np
+import pandas as pd
+import random
+import time
+def set_job_list_arrival_time(job_list, arrival_rate=None, interval=60, shuffle_order=False):
+    """
+    job_list: jobs to execute in this run
+    arrival_rate: num of jobs to arrive at each time interval (-1 or None means no changes)
+    interval: time interval (default: 60)
+    shuffle_order: bool, whether each user's inherent job order are shuffled (default: False)
+    """
+    if arrival_rate is None or arrival_rate < 0:
+        return 0  # respect the original submit time
+    if shuffle_order is True:
+        np.random.shuffle(job_list)
+    else:
+        job_list.sort(key=lambda e: (e.get('submit_time', float('inf')), e['job_id']))
+
+    arrival_counter = 0
+    for job in job_list:
+        arrival_time = (arrival_counter // arrival_rate) # * interval
+        job['submit_time'] = arrival_time
+        arrival_counter += 1
+    
+    return job_list
+
+def _add_job(job_list, job_dict, describe_dict=None):
+    # Add job (job_dict) into job_list
+    for key, value in job_dict.items():
+        if value is not None and value.isdigit() and key != 'user':
+            if type(value) == str:
+                job_dict[key] = round(float(value))
+            else:  # duration becomes an int
+                job_dict[key] = round(value)
+        elif key in ['wait_time','user_dur','user_gpu_dur','group_dur','group_gpu_dur']:
+            try:
+                job_dict[key] = float(value)
+            except:
+                pass
+
+    keys = ['num_cpu', 'num_gpu', 'submit_time', 'num_inst']
+    for key in keys:
+        if key not in job_dict or job_dict[key] == '':
+            if key in ['num_cpu', 'num_gpu']:
+                job_dict[key] = 0
+            else:  # key in ['submit_time', 'num_inst']
+                job_dict[key] = 1
+        else:
+            if key in ['num_cpu', 'num_gpu']:  # in %
+                job_dict[key] = float(job_dict[key]) #round(100 * float(job_dict[key]))
+            else:
+                job_dict[key] = round(float(job_dict[key]))
+        if key == 'num_inst':
+            job_dict[key] = 1
+
+
+    # Add entries to be used in scheduling
+    job_dict['duration'] = int(float(job_dict['duration']))
+    # job_dict['duration'] = 1
+    # if job_dict['duration'] <= 0:
+    #     job_dict['duration'] = 1  # fix duration == 0 problem.
+    job_dict['size'] = int((job_dict['num_gpu'] + job_dict['num_cpu']) * job_dict['duration']) # (gpu + cpu) x duration
+    job_dict['on_time'] = 0
+    job_dict['wasted'] = 0
+    job_dict['jct'] = -1
+    job_dict['resource'] = [job_dict['num_gpu'], job_dict['num_cpu']] # list of resources
+    job_dict['node'] = None
+
+    # Add duration estimation
+    if describe_dict is not None:
+        jd_user = describe_dict.get(job_dict['user'])
+        if jd_user is not None:
+            job_dict['dur_avg'] = float(jd_user['mean'])  # expectation
+            job_dict['dur_std'] = float(jd_user['std'])  # standard deviation
+            job_dict['dur_med'] = float(jd_user['50%'])  # median
+            job_dict['dur_trim_mean'] = float(jd_user['trim_mean'])  # discard 10% top and 10% tail when calc. mean
+
+    # Remove original unused entries
+    for drop_col in ['fuxi_job_name','fuxi_task_name','inst_id','running_cluster','model_name','iterations','interval','vc','jobid','status']:
+        if drop_col in job_dict: job_dict.pop(drop_col)
+    
+    job_dict['allocated_at'] = 0
+
+    if job_dict['num_gpu'] != 0:
+        if job_dict['gpu_type'] == 'MISC':
+            if job_dict['num_gpu'] <= 8 and job_dict['num_cpu'] <96:
+                job_list.append(job_dict)
+            # else:
+            #     print(job_dict)
+        if job_dict['gpu_type'] == 'P100':
+            if job_dict['num_gpu'] <= 2 and job_dict['num_cpu'] <64:
+                job_list.append(job_dict)
+            # else:
+            #     print(job_dict)
+        if job_dict['gpu_type'] == 'T4':
+            if job_dict['num_gpu'] <= 2 and job_dict['num_cpu'] <96:
+                job_list.append(job_dict)
+            # else:
+            #     print(job_dict)
+        if job_dict['gpu_type'] == 'V100':
+            if job_dict['num_gpu'] <= 8 and job_dict['num_cpu'] <96:
+                job_list.append(job_dict)
+            # else:
+            #     print(job_dict)
+    # job_list.append(job_dict)
+    
+
+
+def add_job(csv_file, describe_dict, limit=None):
+    """
+    limit: To avoid reading too many jobs when the sampled number << total number of jobs in trace file.
+    """
+    job_list = []
+    with open(csv_file, 'r') as fd:
+        reader = csv.DictReader(fd, delimiter=',')
+        keys = reader.fieldnames
+        for i, row in enumerate(reader):
+            _add_job(job_list, row, describe_dict)
+            # if limit is not None and i >= 1000:
+            #     break
+    return job_list
+
+
+
+
+def init_go_(num_jobs, arrivals):
+    random.seed(0)
+    cur_time = 0
+    csv_file='/home/tst/Documents/clusterdata/cluster-trace-gpu-v2020/simulator/traces/pai/pai_job_no_estimate_100K.csv'
+    job_list = add_job(csv_file, None, limit=num_jobs)
+    print('job_list size:')
+    print(len(job_list))
+    if (num_jobs is not None) and num_jobs <= len(job_list):
+        random.seed(time.time())
+        random.shuffle(job_list)
+
+        job_list = job_list[:num_jobs]
+    job_list = set_job_list_arrival_time(job_list, arrivals)
+    return job_list
+
+
+
+# jobs = init_go()
+
+# df = pd.DataFrame(jobs)
+
+# print(df.describe())
